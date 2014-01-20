846bebc6fed23fb38824b1299139a1de085965bd
philipp <neubauer.phil@gmail.com>
2014-01-21 10:08:56 +1300

    migrate

---
+++ .emacs.d/elpa/popup-20130901.2255/popup-autoloads.el
@@ -0,0 +1,18 @@
+;;; popup-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads nil nil ("popup-pkg.el" "popup.el") (21167 28453
+;;;;;;  422289 956000))
+
+;;;***
+
+(provide 'popup-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; popup-autoloads.el ends here
